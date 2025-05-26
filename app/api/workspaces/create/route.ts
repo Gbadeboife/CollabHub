016f3779cc3ee43dbeec779ace2e7dbef2f21602
Gbadebo Ifedayo <gbadeboifedayo@gmail.com>
2025-05-26 13:09:21 +0100

    Update route.ts

--- app/api/workspaces/create/route.ts
+++ app/api/workspaces/create/route.ts
@@ -2,23 +2,41 @@ import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 
 export async function POST(request: Request) {
+
+    interface WorkspaceData {
+        id?: number;
+        name: string;
+        description: string;
+        iconSrc: string;
+        members: number[];
+        channels: number[];
+    }
+
     try{
         const supabase = await createClient();
         const workspaceData = await request.json();
-        const { name, ownerId } = workspaceData;
+        const { name, description, iconSrc, members, channels } = workspaceData;
         
-        const owner_id= ownerId
+        const owner_id= 1
+        const icon= iconSrc
       
 
-        const { data, error } = await supabase
+        const { data , error } = await supabase
         .from('Workspaces')
-        .insert([{ name, owner_id }]) 
+        .insert([{ name, owner_id, description, icon, members, channels }]) 
+        .select() as unknown as { data: WorkspaceData[] | null; error: any };
 
-        if (error) {
-            return NextResponse.json({ error: error.message }, { status: 400 })
+
+        if (error || !data || data.length === 0) {
+            console.error('Insert failed:', error);
+            return;
         }
 
-        return new NextResponse(null, { status: 201 })
+        const insertedWorkspace = data[0];
+        const workspaceId = insertedWorkspace.id;
+        
+
+        return NextResponse.json({id: workspaceId}, { status: 201 })
 
     } catch (error) {
         console.error("Error creating workspace:", error);
