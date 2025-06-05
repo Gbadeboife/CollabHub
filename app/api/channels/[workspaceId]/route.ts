b629ae8fac8f972dd41ff9ff5385486a837b921f
Gbadebo Ifedayo <gbadeboifedayo@gmail.com>
2025-06-05 10:30:36 +0100

    api changes

--- app/api/channels/[workspaceId]/route.ts
+++ app/api/channels/[workspaceId]/route.ts
@@ -2,38 +2,6 @@ import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 
 export async function GET(  req: Request,
-  { params }: { params: { channelId: string } }
-) {
-    try{
-        const { channelId } = params
-
-        const supabase = await createClient();
-
-
-        const { data: channel, error } = await supabase
-        .from('Channels')
-        .select('*')
-        .eq('id', channelId)
-        .single()
-
-        if (error) {
-            return NextResponse.json({ error: error.message }, { status: 404 })
-        }
-
-        return NextResponse.json({ channel }, { status: 200 })
-    } catch (error) {
-        return new NextResponse("Internal Server Error", {
-            status: 500,
-        })
-    }
-
-}
-
-
-import { NextResponse } from "next/server";
-import { createClient } from "@/utils/supabase/server";
-
-export async function GET(  req: Request,
   { params }: { params: { workspaceId: string } }
 ) {
     try{
@@ -50,7 +18,7 @@ export async function GET(  req: Request,
             return NextResponse.json({ error: error.message }, { status: 400 })
         }
 
-        return NextResponse.json({ channels });
+        return NextResponse.json({ channels }, { status: 200 });
 
     } catch (error) {
         return new NextResponse("Internal Server Error", {
