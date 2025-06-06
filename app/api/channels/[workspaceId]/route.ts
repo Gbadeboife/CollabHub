48b784ddbbe0526b326417122f4dfd5f31984def
Gbadebo Ifedayo <gbadeboifedayo@gmail.com>
2025-06-06 17:32:29 +0100

    Update route.ts

--- app/api/channels/[workspaceId]/route.ts
+++ app/api/channels/[workspaceId]/route.ts
@@ -1,11 +1,14 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
+import { NextRequest } from 'next/server'
 
-export async function GET(  req: Request,
-  { params }: { params: { workspaceId: string } }
+
+export async function GET(
+  req: NextRequest,
+  context: { params: { workspaceId: string } }
 ) {
     try{
-        const { workspaceId } = params
+        const { workspaceId } = context.params
 
         const supabase = await createClient();
         
